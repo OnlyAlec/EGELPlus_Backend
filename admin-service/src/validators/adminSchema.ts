73fac07dc1f778d5372d60e6ae0c1fc4cdb65a25
EliB <149275946+EliBece@users.noreply.github.com>
2025-11-20 02:58:36 -0600

    feat(adm-service): :sparkles: Add UpdateUserPermissionsSchema

--- admin-service/src/validators/adminSchema.ts
+++ admin-service/src/validators/adminSchema.ts
@@ -1,5 +1,19 @@
 import * as z from "zod";
 
 /**
- * Schema de ...
+ * Schema para actualizar los permisos de un usuario
  */
+export const UpdateUserPermissionsSchema = z.object({
+  permissions: z
+    .array(
+      z.object({
+        fileId: z.number().int().positive(),
+        permissionTypeId: z.number().int().positive(),
+      })
+    )
+    .min(1, "Debe enviar al menos un permiso."),
+});
+
+export type UpdateUserPermissionsDto = z.infer<
+  typeof UpdateUserPermissionsSchema
+>;
